0bb7eb797ced9066552378377d3a56d70e0968fd
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 16:18:19 +0700

    Create Teks.py

---
+++ module/Teks.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.2)
+mengetik('hallo wellcome to my github MR-Xyaa.')
