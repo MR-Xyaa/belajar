7604bb98aae7767a9a4b094f44e42cdb650d1446
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 19:53:10 +0700

    Update teks.py

--- module/teks.py
+++ module/teks.py
@@ -5,7 +5,11 @@ def mengetik(s):
     for c in s + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(random.random() * 0.5)
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
 
 
 
