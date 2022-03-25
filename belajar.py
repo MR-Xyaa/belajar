48f47bcc037b57683250f2dff3ac4c6433adad55
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 13:51:35 +0700

    Create belajar.py

---
+++ belajar.py
@@ -0,0 +1,27 @@
+import os
+try:
+    import time
+    import random
+    import re
+
+except ImportError:
+    os.system("pip install re")
+
+# -----[ Color ]-----
+m = '\x1b[1;31m'
+p = '\x1b[1;37m'
+h = '\x1b[1;32m'
+b = '\x1b[1;34m' 
+# -----------------
+
+#------------[ Banner ]--------
+banner = f'''
+{m}{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]
+{h}   ||   [▪]{b}Author : MR-Xyaa                                                ||
+{h}   ||   [▪]{b}Github : https://github.com/MR-Xyaa          ||
+{h{   ||   [▪]{b}Facebook :Xyaa Xyaa                                          ||
+{m}{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]'''
+
+# -------------------[ clear ]-----------------------
+
+
