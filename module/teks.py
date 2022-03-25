c68cf547fcc99b799fab23ceeb25783eb74efcb7
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 16:47:49 +0700

    Update teks.py

--- module/teks.py
+++ module/teks.py
@@ -6,7 +6,28 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
 #kecepatan mengetik
-        time.sleep(random.random() * 0.1)
+        time.sleep(random.random() * 0.6)
 mengetik('hallo wellcome to my github MR-Xyaa.')
-mengetik('oke.')
-mengetik('BANGSAT KOE ANJING YAH.')
+mengetik('●●●●●●●●●●●●●●●●●●●●')
+mengetik('please wait...')
+clear
+sleep 5
+mengetik('MENDOWLOAD DATA1...')
+sleep 5
+mengetik('SUKSES')
+mengetik('MENDOWNLOAD DATA2...')
+mengetik('SUKSES')
+sleep 5
+mengetik('MENDOWNLOAD DATA3...')
+mengetik('SUKSES')
+sleep 5
+mengetik('TUNGGU SEBENTAR...')
+
+#profil
+clear
+
+mengetik('{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]
+           ■       [•]Author____: MR-Xyaa                                                
+           ■       [•]Github____: https://github.com/MR-Xyaa                             
+           ■       [•]Facebook__: Xyaa Xyaa                                                 
+          {●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]') 
