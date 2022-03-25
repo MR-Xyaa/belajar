2453fd31163a91def6dc56c7bb7887a20ff3cad2
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 16:53:29 +0700

    Update teks.py

--- module/teks.py
+++ module/teks.py
@@ -6,28 +6,24 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
 #kecepatan mengetik
-        time.sleep(random.random() * 0.6)
+        time.sleep(random.random() * 0.5)
 mengetik('hallo wellcome to my github MR-Xyaa.')
 mengetik('●●●●●●●●●●●●●●●●●●●●')
 mengetik('please wait...')
 clear
-sleep 5
 mengetik('MENDOWLOAD DATA1...')
-sleep 5
 mengetik('SUKSES')
 mengetik('MENDOWNLOAD DATA2...')
 mengetik('SUKSES')
-sleep 5
 mengetik('MENDOWNLOAD DATA3...')
 mengetik('SUKSES')
-sleep 5
 mengetik('TUNGGU SEBENTAR...')
 
-#profil
-clear
+           #profil
+     clear
 
-mengetik('{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]
-           ■       [•]Author____: MR-Xyaa                                                
-           ■       [•]Github____: https://github.com/MR-Xyaa                             
-           ■       [•]Facebook__: Xyaa Xyaa                                                 
-          {●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]') 
+mengetik('{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]')
+mengetik('_■       [•]Author____: MR-Xyaa                                            ')    
+mengetik('_■       [•]Github____: https://github.com/MR-Xyaa                         ')    
+mengetik('_■       [•]Facebook__: Xyaa Xyaa                                          ')       
+mengetik('{●]▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪[●]') 
